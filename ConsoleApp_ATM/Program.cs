67541c8bd86a2769cdf7d3f7212d7d6ce314a95e
xiebo2008 <xiebo2008@yeah.net>
2016-03-18 10:11:05 +0800

    增加了 开户功能，并有静态量进行存储

--- ConsoleApp_ATM/Program.cs
+++ ConsoleApp_ATM/Program.cs
@@ -8,15 +8,28 @@ namespace ConsoleApp_ATM
 {
     class Program
     {
+        static string g_username;
+        static string g_password;
+        static int g_balance=0;
+        static int add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
         static void Main()
         {
+           // Console.WriteLine("请输入A,B整数");
+           //int a = int.Parse( Console.ReadLine());
+           //int b = int.Parse(Console.ReadLine());
+           // int c = add(a, b);
+            //Console.WriteLine("最后的结果:" + c.ToString());
+
             while (true)
             {
                 Console.WriteLine("欢迎来到2015级 ATM 系统");
                 Console.WriteLine("1 登录 2 开户");
                 string input = Console.ReadLine();
                 int choice = 0;
-
                 bool result = int.TryParse(input, out choice);
                 if (result)
                 {
@@ -38,21 +51,48 @@ namespace ConsoleApp_ATM
 
         }
 
+      
+
         private static void Create()
         {
-            Console.WriteLine("请输入新账号");
+            Console.WriteLine("请输入新账号");          
+            g_username= Console.ReadLine();
+            Console.WriteLine("请输入密码");
+            g_password = Console.ReadLine();
+            g_balance = 100;
+            Console.WriteLine("开户成功.....按任意键退出");
             Console.ReadLine();
+           // return 0;
         }
 
-        private static void Login()
+     
+        static void Login()
         {
-            Console.WriteLine("Please input your account");
-            Console.ReadLine();
+            Console.WriteLine("请输入账号");
+            string username = Console.ReadLine();
+            Console.WriteLine("请输入密码");
+            string password = Console.ReadLine();
+            if (username == g_username && password == g_password)
+            {
+                Console.WriteLine("登录成功");
+                Console.WriteLine("您的余额是{0}", g_balance);
+                GotoSecondMenu();
+
+            }
+            else
+            {
+                Console.WriteLine("登录失败，请重新登录");
+            }
             //if you   successe to login
             //show the second menu
             // 1  Save 2 Withdraw 3 transfer 4 detail(last 10 records) 5 logout 
 
         }
 
+        private static void GotoSecondMenu()
+        {
+
+            Console.WriteLine("1存款 2取款 3转账 4查流水 5回到上级");
+        }
     }
 }
