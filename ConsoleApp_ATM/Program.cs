6755887a1fa7638a604644a02f9089c18f545285
xiebo2008 <xiebo2008@yeah.net>
2016-03-22 15:55:02 +0800

    加入了结构体来组织单元信息，建立 建模的概念

--- ConsoleApp_ATM/Program.cs
+++ ConsoleApp_ATM/Program.cs
@@ -1,32 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
 namespace ConsoleApp_ATM
 {
     class Program
     {
-        static string g_username;
-        static string g_password;
-        static int g_balance=0;
-        static int add(int a, int b)
-        {
-            int c = a + b;
-            return c;
-        }
+        // 定义了一个Account 类型变量
+       static Account  g_account;
+    //    int[] arr = new int[6] ;
+        static Account[] arrAccount = new Account[10];
+        
         static void Main()
-        {
-           // Console.WriteLine("请输入A,B整数");
-           //int a = int.Parse( Console.ReadLine());
-           //int b = int.Parse(Console.ReadLine());
-           // int c = add(a, b);
-            //Console.WriteLine("最后的结果:" + c.ToString());
-
+        {           
             while (true)
             {
-                Console.WriteLine("欢迎来到2015级 ATM 系统");
+                Console.WriteLine("欢迎来到九职大2015级 ATM 系统");
                 Console.WriteLine("1 登录 2 开户");
                 string input = Console.ReadLine();
                 int choice = 0;
@@ -42,24 +30,18 @@ namespace ConsoleApp_ATM
                             Create();
                             break;
                         default: break;
-
                     }
                 }
             }
-
-
-
         }
-
-      
 
         private static void Create()
         {
             Console.WriteLine("请输入新账号");          
-            g_username= Console.ReadLine();
+            g_account.username= Console.ReadLine();
             Console.WriteLine("请输入密码");
-            g_password = Console.ReadLine();
-            g_balance = 100;
+            g_account.password = Console.ReadLine();
+            g_account.balance = 100;
             Console.WriteLine("开户成功.....按任意键退出");
             Console.ReadLine();
            // return 0;
@@ -72,21 +54,17 @@ namespace ConsoleApp_ATM
             string username = Console.ReadLine();
             Console.WriteLine("请输入密码");
             string password = Console.ReadLine();
-            if (username == g_username && password == g_password)
+            if (username == g_account.username && password == g_account.password)
             {
                 Console.WriteLine("登录成功");
-                Console.WriteLine("您的余额是{0}", g_balance);
+                Console.WriteLine("您的余额是{0}", g_account.balance);
                 GotoSecondMenu();
-
             }
             else
             {
                 Console.WriteLine("登录失败，请重新登录");
             }
-            //if you   successe to login
-            //show the second menu
-            // 1  Save 2 Withdraw 3 transfer 4 detail(last 10 records) 5 logout 
-
+          
         }
 
         private static void GotoSecondMenu()
