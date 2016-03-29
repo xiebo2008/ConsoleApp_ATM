a0215349866bc3a96b2dbe5e2c1c65972c05e0ba
xiebo2008 <xiebo2008@yeah.net>
2016-03-29 16:52:41 +0800

    完善注释， 加入一个IsExist 方法判断用户名是否存在，用于用户名注册

--- ConsoleApp_ATM/Program.cs
+++ ConsoleApp_ATM/Program.cs
@@ -5,13 +5,15 @@ namespace ConsoleApp_ATM
 {
     class Program
     {
-      static Account currentUser;
-        //使用泛型列表
+        //存储当前登录用户
+       static Account currentUser;
+        //使用泛型列表来存储多用户
         static List<Account> lstAccount = new List<Account>();
 
 
         static void Main()
         {
+            //初始化用户
             Init();
             while (true)
             {
@@ -36,10 +38,10 @@ namespace ConsoleApp_ATM
             }
         }
 
-        //初始化函数， 会在程序的一开始进行调用
+        //初始化函数， 在程序的一开始进行调用
         private static void Init()
         {
-            Console.WriteLine(lstAccount.Count);
+           
             Account a1 = new Account();
             a1.username = "xiebo";
             a1.password = "123";
@@ -51,35 +53,55 @@ namespace ConsoleApp_ATM
             a2.password = "123";
             a2.balance = 200;
             lstAccount.Add( a2);
-            Console.WriteLine("初始化结束");
-            Console.WriteLine(lstAccount.Count);
+            Console.WriteLine("初始化结束，已初始化{0}个用户",lstAccount.Count);
+            
 
+        }
+        /// <summary>
+        /// 判断用户是否已经存在于用户列表中， 如果不存在返回false， 存在则返回true
+        /// </summary>
+        /// <param name="username"></param>
+         /// <returns></returns>
+        static bool IsExist(string username)
+        {
+            for (int index = 0; index < lstAccount.Count; index++)
+            {
+                if (lstAccount[index].username == username)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         private static void Create()
         {
-            Console.WriteLine("请输入新账号");
-            string username = Console.ReadLine();
-            Console.WriteLine("请输入密码");
-            string password = Console.ReadLine();
-            Account a = new Account();
-            a.username = username;
-            a.password = password;
-            a.balance = 100;
+           
+            while (true)
+            {
+                Console.WriteLine("请输入新账号");
+                string username = Console.ReadLine();
+                if (!IsExist(username))
+                {
+                    Console.WriteLine("请输入密码");
+                    string password = Console.ReadLine();
+                    Account a = new Account();
+                    a.username = username;
+                    a.password = password;
+                    a.balance = 100;
+                    lstAccount.Add(a);
+                    Console.WriteLine("开户成功.....按任意键退出");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("该帐号已经存在");                
 
-            lstAccount.Add( a);
-            //for (int index = 0; index < lstAccount.Count; index++)
-            //{
-            //    if (lstAccount[index].username == null)
-            //    {
-            //        lstAccount[index] = a;
-            //    }
-            //}
+                }
+            }
 
 
-            Console.WriteLine("开户成功.....按任意键退出");
-            Console.ReadLine();
-          
         }
 
 
@@ -89,16 +111,20 @@ namespace ConsoleApp_ATM
             string username = Console.ReadLine();
             Console.WriteLine("请输入密码");
             string password = Console.ReadLine();
+            bool isValid = false;
             for (int index = 0; index < lstAccount.Count; index++)
             {
                 if (username == lstAccount[index].username && password == lstAccount[index].password)
                 {
                     Console.WriteLine("登录成功");
                     Console.WriteLine("您的余额是{0}", lstAccount[index].balance);
+                    isValid = true;
                     GotoSecondMenu();
                 }
             } 
-            Console.WriteLine("登录失败，请重新登录");
+            if (!isValid )
+            { Console.WriteLine("登录失败，请重新登录"); }
+           
             
 
         }
