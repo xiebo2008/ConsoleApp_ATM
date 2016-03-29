131fc3a19063bddd5ab9287ffb2b975f1e2e77b0
xiebo2008 <xiebo2008@yeah.net>
2016-03-29 16:04:49 +0800

    使用泛型链表来存储多用户

--- ConsoleApp_ATM/Program.cs
+++ ConsoleApp_ATM/Program.cs
@@ -1,17 +1,18 @@
 ﻿
 using System;
-
+using System.Collections.Generic;
 namespace ConsoleApp_ATM
 {
     class Program
     {
-        // 定义了一个Account 类型变量
-       static Account  g_account;
-    //    int[] arr = new int[6] ;
-        static Account[] arrAccount = new Account[10];
-        
+      static Account currentUser;
+        //使用泛型列表
+        static List<Account> lstAccount = new List<Account>();
+
+
         static void Main()
-        {           
+        {
+            Init();
             while (true)
             {
                 Console.WriteLine("欢迎来到九职大2015级 ATM 系统");
@@ -35,42 +36,78 @@ namespace ConsoleApp_ATM
             }
         }
 
-        private static void Create()
+        //初始化函数， 会在程序的一开始进行调用
+        private static void Init()
         {
-            Console.WriteLine("请输入新账号");          
-            g_account.username= Console.ReadLine();
-            Console.WriteLine("请输入密码");
-            g_account.password = Console.ReadLine();
-            g_account.balance = 100;
-            Console.WriteLine("开户成功.....按任意键退出");
-            Console.ReadLine();
-           // return 0;
+            Console.WriteLine(lstAccount.Count);
+            Account a1 = new Account();
+            a1.username = "xiebo";
+            a1.password = "123";
+            a1.balance = 100;
+            lstAccount.Add( a1);
+
+            Account a2 = new Account();
+            a2.username = "xiexie";
+            a2.password = "123";
+            a2.balance = 200;
+            lstAccount.Add( a2);
+            Console.WriteLine("初始化结束");
+            Console.WriteLine(lstAccount.Count);
+
         }
 
-     
+        private static void Create()
+        {
+            Console.WriteLine("请输入新账号");
+            string username = Console.ReadLine();
+            Console.WriteLine("请输入密码");
+            string password = Console.ReadLine();
+            Account a = new Account();
+            a.username = username;
+            a.password = password;
+            a.balance = 100;
+
+            lstAccount.Add( a);
+            //for (int index = 0; index < lstAccount.Count; index++)
+            //{
+            //    if (lstAccount[index].username == null)
+            //    {
+            //        lstAccount[index] = a;
+            //    }
+            //}
+
+
+            Console.WriteLine("开户成功.....按任意键退出");
+            Console.ReadLine();
+          
+        }
+
+
         static void Login()
         {
             Console.WriteLine("请输入账号");
             string username = Console.ReadLine();
             Console.WriteLine("请输入密码");
             string password = Console.ReadLine();
-            if (username == g_account.username && password == g_account.password)
+            for (int index = 0; index < lstAccount.Count; index++)
             {
-                Console.WriteLine("登录成功");
-                Console.WriteLine("您的余额是{0}", g_account.balance);
-                GotoSecondMenu();
-            }
-            else
-            {
-                Console.WriteLine("登录失败，请重新登录");
-            }
-          
+                if (username == lstAccount[index].username && password == lstAccount[index].password)
+                {
+                    Console.WriteLine("登录成功");
+                    Console.WriteLine("您的余额是{0}", lstAccount[index].balance);
+                    GotoSecondMenu();
+                }
+            } 
+            Console.WriteLine("登录失败，请重新登录");
+            
+
         }
 
         private static void GotoSecondMenu()
         {
 
             Console.WriteLine("1存款 2取款 3转账 4查流水 5回到上级");
+            Console.ReadLine();
         }
     }
 }
